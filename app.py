ed0d68b1f6bd5b4868897d300fe6ef3c715a7867
w3_jrodrigues <joao.paulo@w3as.com.br>
2024-08-27 13:40:42 -0300

    add cors

--- app.py
+++ app.py
@@ -6,27 +6,29 @@ from dotenv import load_dotenv
 import cachetools
 import os
 from gemini2 import gerar_resumo
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
+# Configuração do CORS
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 try:
-    client = MongoClient(os.getenv('MONGO_URI'), serverSelectionTimeoutMS=5000)  # Timeout para conexão
+    client = MongoClient(os.getenv('MONGO_URI'), serverSelectionTimeoutMS=5000)
     db = client[os.getenv('DATABASE_NAME')]
     collection = db[os.getenv('COLLECTION_NAME')]
-    # Testar a conexão
-    client.server_info()
+    client.server_info()  # Testar a conexão
 except ServerSelectionTimeoutError:
     print("Erro ao conectar ao MongoDB")
     collection = None
 
-# Configuração do cache
-cache = cachetools.TTLCache(maxsize=100, ttl=3600)  # Armazena resumos por produto por 1 hora
+cache = cachetools.TTLCache(maxsize=100, ttl=3600)  # Cache configurado para 1 hora
 
 def filtrar_comentarios(reviews):
-    return [avaliacao['avaliacao'] for avaliacao in reviews[:20]]  # Pega apenas os primeiros 20 itens
+    return [avaliacao['avaliacao'] for avaliacao in reviews[:20]]
 
 @app.route('/save_review', methods=['POST'])
 def save_review():
@@ -34,55 +36,38 @@ def save_review():
     Adiciona uma nova avaliação para um produto no banco de dados.
     ---
     tags:
-      - Avaliações
+      - Reviews
     parameters:
       - name: body
         in: body
         required: true
         schema:
           type: object
+          required:
+            - produto_id
+            - nome_usuario
+            - nota
+            - avaliacao
           properties:
             produto_id:
               type: string
-              description: ID do produto que está sendo avaliado
-              example: produto123
+              description: O ID do produto.
             nome_usuario:
               type: string
-              description: Nome do usuário que faz a avaliação
-              example: Ana
+              description: Nome do usuário que faz a avaliação.
             nota:
               type: integer
-              description: Nota da avaliação (de 1 a 5)
-              example: 5
+              description: Nota dada ao produto.
             avaliacao:
               type: string
-              description: Texto da avaliação
-              example: Excelente!
+              description: Comentário sobre o produto.
     responses:
       201:
-        description: Avaliação salva com sucesso
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              example: Avaliação salva com sucesso
+        description: Avaliação salva com sucesso.
       400:
-        description: Dados inválidos
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              example: Dados inválidos
+        description: Dados inválidos.
       500:
-        description: Erro de conexão com o banco de dados
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              example: Erro de conexão com o banco de dados
+        description: Erro de conexão com o banco de dados.
     """
     if collection is None:
         return jsonify({'error': 'Erro de conexão com o banco de dados'}), 500
@@ -112,48 +97,31 @@ def get_reviews():
     Retorna todas as avaliações para um produto específico e o resumo das avaliações.
     ---
     tags:
-      - Avaliações
+      - Reviews
     parameters:
       - name: produto_id
         in: query
-        required: true
         type: string
-        description: ID do produto para o qual as avaliações são solicitadas
-        example: produto123
+        required: true
+        description: O ID do produto.
     responses:
       200:
-        description: Lista de avaliações e o resumo das avaliações
+        description: Uma lista de avaliações e o resumo das avaliações.
         schema:
           type: object
           properties:
             resumo_avaliacao:
               type: string
-              example: "Nenhum resumo disponível."
+            media:
+              type: number
             avaliacoes:
               type: array
               items:
                 type: object
-                properties:
-                  produto_id:
-                    type: string
-                    example: produto123
-                  nome_usuario:
-                    type: string
-                    example: Ana
-                  nota:
-                    type: integer
-                    example: 5
-                  avaliacao:
-                    type: string
-                    example: Excelente!
+      400:
+        description: ID do produto é obrigatório.
       500:
-        description: Erro de conexão com o banco de dados
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              example: Erro de conexão com o banco de dados
+        description: Erro de conexão com o banco de dados.
     """
     if collection is None:
         return jsonify({'error': 'Erro de conexão com o banco de dados'}), 500
@@ -163,7 +131,7 @@ def get_reviews():
     if not produto_id:
         return jsonify({'error': 'ID do produto é obrigatório'}), 400
 
-    reviews = list(collection.find({'produto_id': produto_id}, {'_id': 0}).sort('data', -1))  # Exclui o campo _id dos resultados
+    reviews = list(collection.find({'produto_id': produto_id}, {'_id': 0}).sort('data', -1))
 
     if not reviews:
         return jsonify({
